053d351d4c278983a556612279990259210b69e4
AtheeshRathnaweera27 <rathnaweeraatheesh72@gmail.com>
2019-06-03 23:50:14 +0530

    Create the get popular deals function

--- pizza_hut_scrap.py
+++ pizza_hut_scrap.py
@@ -9,7 +9,6 @@ import json
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
-
 #launch url
 url = "https://www.pizzahut.lk/home"
 
@@ -18,9 +17,9 @@ labelList = ["PIZZAS"]
 # create a new Firefox session
 
 #Add option for headless browsing
-options = FirefoxOptions()
-options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
+#options = FirefoxOptions()options=options
+#options.add_argument("--headless")
+driver = webdriver.Firefox()
 
 #driver = webdriver.Edge()
 driver.implicitly_wait(30)
@@ -31,82 +30,102 @@ soup_level1=BeautifulSoup(driver.page_source, 'lxml')
 x=1#counter
 divsList = []
 
-for index in labelList:
+def getThePopularDeals():
+    popularDealsListImages = []
 
-    pizzaDataList = [] #Empty list for store pizza data
+    promoDivs = soup_level1.find_all("div",attrs={"class":"col-md-6 col-sm-12 col-xs-12 promo-single promo-add"})
 
-    button = driver.find_element_by_link_text(index)
-    print (str(x)+" "+button.get_attribute("href"))
-    button.click()#go to the link
+    numberOfpopularDeals = len(promoDivs)
+    print("Number of popular deals: "+str(numberOfpopularDeals))
 
-    tempPage=BeautifulSoup(driver.page_source, 'lxml')#Selenium hands of the source of the specific job page to Beautiful Soup
+    for promos in promoDivs:
 
-    #subMenuCategories = tempPage.find_all("ul",attrs={"class": "sub-menu-categories-ul"})#get sub category links and loop through each
+        singleImage = promos.find("img",attrs={"class","img img-responsive"})
+        popularDealsListImages.append(singleImage['src'])
+    
 
-    for subMenu in tempPage.find_all("ul",attrs={"class": "sub-menu-categories-ul"}):
+    print("Number of images : "+str(len(popularDealsListImages)))
+    imageJson = json.dumps(popularDealsListImages)
+    print(" JSOn : "+imageJson)
 
-        subMenuLinks = subMenu.find_all("li")
-        numberOfLinksInSubMenu = len(subMenuLinks)
-        print(numberOfLinksInSubMenu)
 
-        for i,list in enumerate(subMenuLinks):
-            a = list.find('a')  
+getThePopularDeals()
 
-            #print(a.get_attribute("class"))
-            print("\n\t\t\t"+a['href'],a.get_text())
 
-            subMenuPage = BeautifulSoup(driver.page_source,'lxml')#get the current page html block
+def getPizzaData():
 
-            singleItemHolder = subMenuPage.find_all("div",attrs={"class" : "box"})
-            print (len(singleItemHolder))
+    for index in labelList:
 
-            for box in singleItemHolder:
+        pizzaDataList = [] #Empty list for store pizza data
+
+        button = driver.find_element_by_link_text(index)
+        print (str(x)+" "+button.get_attribute("href"))
+        button.click()#go to the link
+
+        tempPage=BeautifulSoup(driver.page_source, 'lxml')#Selenium hands of the source of the specific job page to Beautiful Soup
+
+        #subMenuCategories = tempPage.find_all("ul",attrs={"class": "sub-menu-categories-ul"})#get sub category links and loop through each
+
+        for subMenu in tempPage.find_all("ul",attrs={"class": "sub-menu-categories-ul"}):
+
+            subMenuLinks = subMenu.find_all("li")
+            numberOfLinksInSubMenu = len(subMenuLinks)
+            print(numberOfLinksInSubMenu)
+
+            for i,list in enumerate(subMenuLinks):
+                a = list.find('a')  
+
+                #print(a.get_attribute("class"))
+                print("\n\t\t\t"+a['href'],a.get_text())
+
+                subMenuPage = BeautifulSoup(driver.page_source,'lxml')#get the current page html block
+
+                singleItemHolder = subMenuPage.find_all("div",attrs={"class" : "box"})
+                print (len(singleItemHolder))
+
+                for box in singleItemHolder:
                
-                itemName = box.find("h3",attrs={"class":"menu-item-name hidden-lg hidden-md"})#get the name
-                itemImageSrc = box.find("img",attrs={"alt":"image-name"})#get the image src
-                itemDescription = box.find("p",attrs={"class":"hidden-xs hidden-sm menu-item-desc text-left"})#get the description
+                    itemName = box.find("h3",attrs={"class":"menu-item-name hidden-lg hidden-md"})#get the name
+                    itemImageSrc = box.find("img",attrs={"alt":"image-name"})#get the image src
+                    itemDescription = box.find("p",attrs={"class":"hidden-xs hidden-sm menu-item-desc text-left"})#get the description
 
-                pizzaData = {
+                    pizzaData = {
                         'name': itemName.get_text(),
                         'src': itemImageSrc['src'],
                         'desc': itemDescription.get_text()
                             }
                 
-                pizzaData_json = json.dumps(pizzaData)#create a json string
-
-                pizzaDataList.append(pizzaData_json)
+                    pizzaData_json = json.dumps(pizzaData)#create a json string
+                    pizzaDataList.append(pizzaData_json)
 
 
             if i < (numberOfLinksInSubMenu-1): #handling the index out of range
                 nextLink = subMenuLinks[i+1].find('a')
                 driver.find_element_by_link_text(nextLink.get_text().upper()).click()
            
-                #leave from the loop
+            #leave from the loop
+                
+        x += 1
 
+    print("\nNumber of pizzas: "+str(len(pizzaDataList)))
 
-   
+    pizzaDataFinalJson = json.dumps(pizzaDataList)
 
-    x += 1
+    #print ("\n"+ pizzaDataFinalJson)
 
-print("\nNumber of pizzas: "+str(len(pizzaDataList)))
+    path = os.getcwd()
 
-pizzaDataFinalJson = json.dumps(pizzaDataList)
+    #open, write, and close the file
+    f = open(path + "\\pizza_data_json.json","w") #FHSU
+    f.write(pizzaDataFinalJson)
+    f.close()
 
-#print ("\n"+ pizzaDataFinalJson)
+    print("All done")
+    driver.quit()#quit the driver
 
-path = os.getcwd()
+    #go to all the links in order now part
 
-#open, write, and close the file
-f = open(path + "\\pizza_data_json.json","w") #FHSU
-f.write(pizzaDataFinalJson)
-f.close()
-
-print("All done")
-driver.quit()#quit the driver
-
-#go to all the links in order now part
-
-#python_button = driver.find_element_by_link_text('PIZZAS') #Go to the pizzas page
-#python_button.click()
+    #python_button = driver.find_element_by_link_text('PIZZAS') #Go to the pizzas page
+    #  python_button.click()
 
 
--- test_json.py
+++ test_json.py
@@ -1,18 +1,48 @@
 import json
 import os
 
+
 pizzaDict = {
-    'name': 'Cheese Pizza',
-    'description': 'This is a cheese pizza',
-    'price': 'Rs.890'
+        'name': 'Cheese Pizza',
+        'description': 'This is a cheese pizza',
+        'price': 'Rs.890'
 }
 
-pizza_json = json.dumps(pizzaDict)
-print (pizzaDict)
+    
 
-path = os.getcwd()
-f = open(path + "\\pizza_json.json","w") #FHSU
-f.write(pizza_json)
-f.close()
+def createTheJson(pizzaDict):
+    pizza_json = json.dumps(pizzaDict)
+    return pizza_json
 
-print(path)
+def printTheJson(jsonString):
+    print (jsonString)
+
+def createAndSaveTheFile(pizza_json):
+    path = os.getcwd()
+    f = open(path + "\\pizza_json.json","w") #FHSU
+    f.write(pizza_json)
+    f.close()
+
+    print(path)
+    
+
+theJson = createTheJson(pizzaDict = pizzaDict)
+printTheJson(jsonString = theJson)
+createAndSaveTheFile(pizza_json = theJson)
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
